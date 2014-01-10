4073c23f2059fefe0e04e37d8897d58622423c8c
michele22 <miih.dance22@hotmail.com>
2014-01-10 17:17:47 -0200

    Create remover.php

---
+++ cadastro/remover.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("menu.php");
+
+    session_start();
+    
+    if(!isset($_SESSION["cadastros"])){
+       echo "Não existem pessoas para remover";
+    }
+    else{
+        $id = $_REQUEST["id"];
+        
+        $cadastros =& $_SESSION["cadastros"];
+        $cadastros[$id] = null;   
+       
+        echo "Remoção efetuado com sucesso!";
+    }
+
+
+?>
