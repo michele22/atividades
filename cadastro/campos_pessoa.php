d91b0410af483dd5f314d9316f2ae1b61f00d6be
michele22 <miih.dance22@hotmail.com>
2014-01-10 17:19:55 -0200

    Create campos_pessoa.php

---
+++ cadastro/campos_pessoa.php
@@ -0,0 +1,23 @@
+<label>Nome: </label>
+<input type="text" name="nome" 
+<br/>
+<Input type="radio" name="sexo" value="m"/>
+<label>Maculino</label>
+<input type="radio" name="sexo" value="f" checked="checked" />
+<label>Feminino</label>
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Aceito o contrato!</label>       
+<br/>
+<label>Estado</label>
+<select name="estado">
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+    <option>Bahia</option>
+    <option>Alagoas</option>
+</select>
+<br/><br/>
+<label>Observações</label>
+<br/>
+<textarea name="observaçoes" rows="8" cols="20"></textarea>
+<br/>
