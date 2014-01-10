16a5ca84e02c7d422d53cb2064a935a18eb2328d
michele22 <miih.dance22@hotmail.com>
2014-01-10 17:54:31 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "O dia de hoje é";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "O dia de hoje é $dia/$mes/$ano";
+echo "<br/>";
+
+?>
