3574cc468135b450580c5daa5b589ca3f1e96ac0
michele22 <miih.dance22@hotmail.com>
2014-01-13 18:29:22 -0200

    Create campos_produtos.php

---
+++ atividade3/campos_produtos.php
@@ -0,0 +1,24 @@
+<label>Nome do Produto: </label>
+<input type="text" name="nome" 
+<br/><br/>
+<Input type="radio" name="cor" value="Amarelo"/>
+<label>Amarelo</label>
+<input type="radio" name="cor" value="Roxo" checked="checked" />
+<label>Roxo</label>
+<input type="radio" name="cor" value=""/>
+<label>Verde</label>
+<br/><br/>
+<label>Prazo de Entrega</label>
+<select name="prazo">
+    <option>5 dias</option>
+    <option>10 dias</option>
+    <option>15 dias</option>
+</select>
+
+<input type="checkbox" name="aceito" value="sim" />
+<label>Aceito o produto!</label>
+<br/><br/>
+<label>Detalhes do Produto:</label>
+<br/>
+<textarea name="Detalhes" rows="10" cols="20"></textarea>
+<br/>
