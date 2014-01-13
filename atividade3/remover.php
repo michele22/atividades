2ff2a659bd4f0975493812e10958a11d9597869d
michele22 <miih.dance22@hotmail.com>
2014-01-13 18:34:19 -0200

    Create remover.php

---
+++ atividade3/remover.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("menu.php");
+
+    session_start();
+    
+    if(!isset($_SESSION["cadastros"])){
+       echo "Não existem produtos para remover";
+    }
+    else{
+        $id = $_REQUEST["id"];
+        
+        $cadastros =& $_SESSION["cadastros"];
+        $cadastros[$id] = null;   
+       
+        echo "Remoção efetuado com sucesso!";
+    }
+
+
+?>
